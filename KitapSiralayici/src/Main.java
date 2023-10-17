9c1b61b5d6f7d948fdc115f56e5adc5260cf8d9c
zeynep avlamaz <143629429+zzavlamaz@users.noreply.github.com>
2023-10-18 01:01:12 +0300

    Main.java

--- KitapSiralayici/src/Main.java
+++ KitapSiralayici/src/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // beş tane nesne oluşturuyoruz
-        Book b1 = new Book("Sineklerin Tanrısı", 224, "William Golding", 1954);
-        Book b2 = new Book("1984", 328, "George Orwell", 1949);
+        Book b1 = new Book("Nietzsche Ağladığında", 416, "Irvin D. Yalom", 1992);
+        Book b2 = new Book("NUTUK", 658, "Mustafa Kemal Atatürk", 1927);
         Book b3 = new Book("Suç ve Ceza", 720, "Fyodor Dostoyevski", 1866);
         Book b4 = new Book("Küçük Prens", 96, "Antoine de Saint-Exupéry", 1943);
-        Book b5 = new Book("Yüzüklerin Efendisi", 1216, "J.R.R. Tolkien", 1954);
+        Book b5 = new Book("İçimizdeki Şeytan", 304, "Sabahattin Ali", 1940);
 
         // Kitapları HashSet içinde saklıyoruz
         HashSet<Book> books = new HashSet<>();
